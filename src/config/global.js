9dcc70da32b62b1a392fcfdbc568226fc4e8a831
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2024-12-06 22:10:51 -0500

    PDF || Descargable

--- src/config/global.js
+++ src/config/global.js
@@ -83,7 +83,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/94160001_CF01_DU.pdf',
       },
       {
         icono: 'fas fa-download',
